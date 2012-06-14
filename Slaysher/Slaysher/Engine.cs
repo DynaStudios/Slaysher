bcac4c4ab714847d043861c2cb4efc242ae280c8
Patrick T. <patrick.trautmann@gmail.com>
2012-06-14 21:20:48 +0200

    Remove blabla comments

--- Slaysher/Slaysher/Engine.cs
+++ Slaysher/Slaysher/Engine.cs
@@ -13,9 +13,6 @@ using Slaysher.Graphics.Camera;
 
 namespace Slaysher
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Engine : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -34,12 +31,6 @@ namespace Slaysher
             _availableScenes = new Dictionary<string, IScene>();
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             //Load SplashScreen as Sample
@@ -56,30 +47,17 @@ namespace Slaysher
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (_activeScene != null)
@@ -90,10 +68,6 @@ namespace Slaysher
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Gray);

40c1efa0fc1f2ec1c297a9635501ccf92d9ce407
Patrick T <patrick.trautmann@gmail.com>
2012-06-23 23:33:29 +0200

    Removed unused using

--- Slaysher/Slaysher/Game/GUI/Menu/MenuComponent.cs
+++ Slaysher/Slaysher/Game/GUI/Menu/MenuComponent.cs
@@ -5,7 +5,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using Slaysher.Game.Physics.Collisions;
 
 namespace Slaysher.Game.GUI.Menu

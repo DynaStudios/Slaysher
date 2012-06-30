a4609f1958e149f02680cb69e6af8c21aaf6119d
Patrick T. <patrick.trautmann@gmail.com>
2012-06-30 07:30:28 +0200

    Minimal Skeletton

---
+++ SlaysherServer/Game/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SlaysherServer.Game.Models
+{
+    public class Client
+    {
+    }
+}
---
+++ SlaysherServer/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using SlaysherServer.Game.Models;
+
+namespace SlaysherServer
+{
+    public class Server
+    {
+        private readonly Socket _listener;
+        private readonly SocketAsyncEventArgs _acceptEventArgs;
+
+        //Server Properties
+        public const int MaxClientConnections = 10;
+
+        public ConcurrentDictionary<int, Client> Clients { get; private set; }
+
+        public Server()
+        {
+            //Network Setup
+            _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            _acceptEventArgs = new SocketAsyncEventArgs();
+            _acceptEventArgs.Completed += AcceptCompleted;
+
+            //Vars Init
+            Clients = new ConcurrentDictionary<int, Client>();
+        }
+
+        private void AcceptCompleted(object sender, SocketAsyncEventArgs e)
+        {
+        }
+    }
+}

b37cf5dba039eb360a6da9404fb6728d07ccb512
Patrick T. <patrick.trautmann@gmail.com>
2012-06-30 16:46:51 +0200

    Handshake Packet Sample

--- SlaysherServer/Network/Handler/PacketHandlers.cs
+++ SlaysherServer/Network/Handler/PacketHandlers.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SlaysherNetworking.Packets;
+using SlaysherNetworking.Packets.Utils;
+using SlaysherServer.Game.Models;
 
 namespace SlaysherServer.Network.Handler
 {
@@ -23,11 +25,23 @@ namespace SlaysherServer.Network.Handler
         public static void Register(PacketType packetID, int length, int minimumLength, OnPacketReceive onReceive)
         {
             m_Handlers[(byte)packetID] = new PacketHandler(packetID, length, minimumLength, onReceive);
+
+            Register(PacketType.Handshake, 4, 0, ReadHandshake);
         }
 
         public static PacketHandler GetHandler(PacketType packetID)
         {
             return m_Handlers[(byte)packetID];
+        }
+
+        public static void ReadHandshake(Client client, PacketReader reader)
+        {
+            HandshakePacket hp = new HandshakePacket();
+            hp.Read(reader);
+
+            if (!reader.Failed)
+            {
+            }
         }
     }
 }

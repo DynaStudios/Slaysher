5b081ba9cceb8b9dc825d238b208e61cbd9dd33f
Patrick T. <patrick.trautmann@gmail.com>
2012-08-04 17:05:58 +0200

    DropDown Progress

---
+++ Slaysher/Slaysher/Game/GUI/Models/DropDownItem.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Slaysher.Game.GUI.Screens;
+
+namespace Slaysher.Game.GUI.Models
+{
+    public class DropDownItem
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+
+        public DropDownItem()
+        {
+            
+        }
+
+        public DropDownItem(int value, string label)
+        {
+            Label = label;
+            Value = value;
+        }
+
+        public void Draw(GameScreen gameScreen, Vector2 position, Vector2 dropdownSize, bool odd = true)
+        {
+            Vector2 labelSize = gameScreen.ScreenManager.Font.MeasureString(Label);
+            labelSize.X = dropdownSize.X;
+
+            Rectangle rec = new Rectangle((int) position.X, (int) position.Y, (int) labelSize.X, (int) labelSize.Y);
+            //Draw Rectangle
+            gameScreen.ScreenManager.SpriteBatch.Draw(gameScreen.ScreenManager.BlankTexture, rec, Color.White);
+            //Draw Font
+            gameScreen.ScreenManager.SpriteBatch.DrawString(gameScreen.ScreenManager.Font, Label, position, Color.White);
+        }
+    }
+}
--- Slaysher/Slaysher/Game/Scenes/TestPanelScreen.cs
+++ Slaysher/Slaysher/Game/Scenes/TestPanelScreen.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Slaysher.Game.GUI.Components;
+using Slaysher.Game.GUI.Models;
 using Slaysher.Game.GUI.Screens;
 
 namespace Slaysher.Game.Scenes
@@ -11,7 +14,18 @@ namespace Slaysher.Game.Scenes
         public override void Activate(bool instancePreserved)
         {
             base.Activate(instancePreserved);
+
+            //Init DropDown
+            DropDownItem item1 = new DropDownItem(1, "Test1");
+            DropDownItem item2 = new DropDownItem(2, "Test2");
+            DropDownItem item3 = new DropDownItem(3, "Test3");
+            List<DropDownItem> dropDownItems = new List<DropDownItem> {item1, item2, item3};
+
+            DropDown dropDown = new DropDown(dropDownItems, item2);
+            dropDown.SelectionChanged += DropDownItemChanged;
+
             PanelEntries.Add(new Label("Hallo Welt") { Color = Color.OrangeRed });
+            PanelEntries.Add(dropDown);
             PanelEntries.Add(new Button("Test Button 1"));
             PanelEntries.Add(new Button("Test Button 2"));
             PanelEntries.Add(new Button("Test Button 3"));
@@ -22,5 +36,11 @@ namespace Slaysher.Game.Scenes
         }
 
         #endregion
+
+        private void DropDownItemChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Item Changed");
+        }
+
     }
 }

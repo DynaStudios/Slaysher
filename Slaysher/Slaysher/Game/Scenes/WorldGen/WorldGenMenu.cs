e9d78dc223f53107a6772297203283da141cbc7f
Patrick T. <patrick.trautmann@gmail.com>
2012-08-14 03:10:33 +0200

    WorldGen Interface + LoadingScreen Changes

---
+++ Slaysher/Slaysher/Game/Scenes/WorldGen/WorldGenMenu.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Slaysher.Game.GUI.Components;
+using Slaysher.Game.GUI.Screens;
+
+namespace Slaysher.Game.Scenes.WorldGen
+{
+    public class WorldGenMenu : WindowScreen
+    {
+        #region Overrides of GameScreen
+
+        public override void Activate(bool instancePreserved)
+        {
+            base.Activate(instancePreserved);
+            PresentationOffset = new Vector2((ScreenManager.GraphicsDevice.Viewport.Width / 4) * 3f, (ScreenManager.GraphicsDevice.Viewport.Height / 4) * 3f);
+        }
+
+        #endregion
+
+        #region Overrides of WindowScreen
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        #endregion
+    }
+}
---
+++ Slaysher/Slaysher/Game/Scenes/WorldGenTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Slaysher.Game.GUI.Components;
+using Slaysher.Game.GUI.Screens;
+using Slaysher.Game.Scenes.WorldGen;
+
+namespace Slaysher.Game.Scenes
+{
+    public class WorldGenTest : GameScreen
+    {
+        #region Overrides of GameScreen
+
+        public override void Activate(bool instancePreserved)
+        {
+            base.Activate(instancePreserved);
+            WorldGenMenu menu = new WorldGenMenu();
+            
+            Button generateButton = new Button("Generate");
+            generateButton.Clicked += GenerateButtonClicked;
+
+            menu.PanelEntries.Add(generateButton);
+
+            ScreenManager.AddScreen(menu);
+
+            //Load other content to generate stuff
+
+            //Render first perlin noise before hiding loading screen...
+        }
+
+        private void GenerateButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Generate clicked!");
+
+            //Create new Perlin noise texture here
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        #endregion
+    }
+}
